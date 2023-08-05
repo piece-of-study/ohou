9b7353f645998845aa68d2e21b6b75e869e2ddcf
JO ARA <jojo00923@gmail.com>
2023-08-05 16:50:28 +0900

    added testController, index.jsp

--- src/main/java/org/ohou/Main.java
+++ src/main/java/org/ohou/Main.java
@@ -1,7 +1,29 @@
 package org.ohou;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    @RequestMapping(value="/")
+    public String index(Model model){
+        model.addAttribute("test", "1234");
+        return "index";
     }
+
+    @RequestMapping(value="/json", produces = "application/json")
+    @ResponseBody
+    public Object json(){
+        Map<String, String> m = new HashMap<>();
+        m.put("test", "1234");
+        return m;
+    }
+//    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//    }
 }
